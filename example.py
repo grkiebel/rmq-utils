3012eac2a30e0864769719fd525b961ddbce153c
grk <grkiebel@gmail.com>
2024-09-23 08:53:03 -0700

    added another example usage

--- example.py
+++ example.py
@@ -11,12 +11,66 @@ from rmq_utils import (
 )
 
 
-def main():
+def get_msg_callback(name: str):
+    def msg_callback(msg: Message):
+        print(
+            f"👉 Reciever {name}: Received message with key {msg.key} and payload {msg.payload}"
+        )
+
+    return msg_callback
+
+
+def post_messages(sender: Sender, keys: list[str]):
+    for i in range(5):
+        for key in keys:
+            post_message(sender, i, key)
+
+
+def post_message(sender, i, key):
+    sender.post_message(Message(key=key, payload=f"Message {i}"))
+    sleep(0.2)
+
+
+def simple_example():
+    """
+    This example demonstrates how to use the rmq_utils package to access the
+    message broker with a sender and a receiver.  The sender sends messages to the
+    exchange, and the receiver receives messages using a fanout routing exchange.
+    """
+    Parameters.set(log_level="WARNING")
+    # Note: Other properties can be set on Parameters as well.
+
+    exchange_name = "bozo"
+    Broker.declare_exchange("main", exchange_name)
+    Connections.close()
+
+    sender = Sender("SND", exchange_name)
+    receiver = Receiver("REC", exchange_name)
+    consumer = Consumer("CNS", receiver.get_queue(), get_msg_callback("REC"))
+
+    sender.start()
+    receiver.start()
+    consumer.start()
+
+    post_message(sender, 1, "REC")
+
+    print("✋🏻 Stopping...")
+    sender.stop()
+    receiver.stop()
+    consumer.stop()
+
+    Broker.delete_exchange(exchange_name)
+
+    print("✅ Done!")
+
+
+def more_complicated_example():
     """
     This example demonstrates how to use the rmq_utils package to create a simple
     message broker with a sender and two receivers.  The sender sends messages to
-    the exchange, and the receivers receive messages from the exchange using
-    direct routing.
+    the exchange, and the receivers receive messages using a direct routing exchange.
+    A runner object is used to simplify the starting and stopping of multiple
+    components.
     """
     Parameters.set(log_level="WARNING")
     # Note: Other properties can be set on Parameters as well.
@@ -41,26 +95,17 @@ def main():
 
     post_messages(sender, receiver_names)
 
-    print("❎ Stopping...")
+    print("✋🏻 Stopping...")
     runner.stop_all()
 
     Broker.delete_exchange(exchange_name)
 
-
-def get_msg_callback(name: str):
-    def msg_callback(msg: Message):
-        print(
-            f"👉 Reciever {name}: Received message with key {msg.key} and payload {msg.payload}"
-        )
-
-    return msg_callback
+    print("✅ Done!")
 
 
-def post_messages(sender: Sender, keys: list[str]):
-    for i in range(5):
-        for key in keys:
-            sender.post_message(Message(key=key, payload=f"Message {i}"))
-            sleep(0.2)
+def main():
+    simple_example()
+    more_complicated_example()
 
 
 if __name__ == "__main__":
